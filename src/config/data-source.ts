8b2462a40533103f70d2a805acb6ba8a22dd02c8
aryanmishra55254 <aryanmishra55254@gmail.com>
2026-04-02 23:49:28 +0530

    connection string based db connection

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -13,11 +13,8 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "5432"),
-    username: process.env.DB_USERNAME || "postgres",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "fds",
+    url: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     synchronize: true,
     logging: false,
     entities: [User, Record, Transaction],
